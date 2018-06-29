6520ba29af73ed17e9dcfbe7889a57c2f64e54c9
wickstjo <33825194+wickstjo@users.noreply.github.com>
2018-06-29 05:55:13 +0300

    Finished frontpage

--- js/app.js
+++ js/app.js
@@ -8,7 +8,21 @@ function log(stuff) {
 $('a#show').on('click', () => {
 
    // PICK UP TARGET HASH
-   var name = $(event.target).attr("name");
+   var path = $(event.target).attr('key');
+
+   // FILE
+   var file = $.ajax({
+      url: "js/content.json",
+      async: false,
+      dataType: 'json'
+   }).responseJSON;
+
+   var instanceData = fetchData(path, file);
+
+   // TRANSFORM OBJECT
+   var stringify = JSON.stringify(file);
+   var minify = vkbeautify.jsonmin(stringify);
+   var beautify = vkbeautify.json(minify, 4);
 
    // GENERATE TABLE
    var selector = `
@@ -16,7 +30,43 @@ $('a#show').on('click', () => {
          <tr>
             <td>
                <div id="prompt-outer">
-                  <div id="prompt-inner">` + name + `</div>
+                  <div id="prompt-header">
+                     <div id="item">
+                        <table>
+                           <tr>
+                              <td>Name/Path: </td>
+                              <td>` + instanceData.path + `</td>
+                           </tr>
+                        </table>
+                        <hr>
+                        <table>
+                           <tr>
+                              <td>Direct Link: </td>
+                              <td>` + instanceData.hash + `</td>
+                           </tr>
+                        </table>
+                        <hr>
+                        <table>
+                           <tr>
+                              <td>Size: </td>
+                              <td>` + instanceData.size + `</td>
+                           </tr>
+                        </table>
+                     </div>
+                  </div>
+
+                  <div id="prompt-inner">
+                     <pre><code class="JSON">` + beautify + `</code></pre>
+                  </div>
+
+                  <div id="prompt-tools">
+                     <table>
+                        <tr>
+                           <td><span id="save">Save</span><span id="upload">Upload</span></td>
+                           <td><a id="asa"><span id="discard">Discard</span></a></td>
+                        </tr>
+                     </table>
+                  </div>
                </div>
             </td>
          </tr>
@@ -25,6 +75,11 @@ $('a#show').on('click', () => {
 
    // PREPEND TO BODY
    $('body').prepend(selector);
+
+   // CODE HIGHLIGHTING
+   $('pre code').each(function(i, block) {
+      hljs.highlightBlock(block);
+   });
 
    // DISPLAY WITH CSS
    $("#prompt").css('display', 'table');
@@ -35,19 +90,9 @@ jQuery(document).on('keyup',function(evt) {
 
    // ESC KEY
    if (evt.keyCode == 27) {
-
-      // CHECK CURRENT DISPLAY VALUE
-      var value = $("#prompt").css('display');
-
-      if (value == 'table') {
-
-         // CSS HIDE SELECTOR
-         $("#prompt").css('display', 'none');
-
-         // REMOVE FROM DOM
-         $('#prompt').remove();
-      }
+      closePrompt();
    }
+
 });
 
 // RENDER FILES
@@ -85,10 +130,10 @@ function renderFiles() {
          // GENERATE ROW
          var row = `
             <tr id="content">
-               <td><a id="show"><div name="` + base.hash + `">
+               <td><a id="show"><div key="` + base.path + `">
                   <table><tr>
-                     <td name="` + base.hash + `">` + base.name + `</a></td>
-                     <td name="` + base.hash + `" id="hash">` + base.hash + `</td>
+                     <td key="` + base.path + `">` + base.name + `</a></td>
+                     <td key="` + base.path + `">` + base.hash + `</td>
                   </tr></table>
                </div></a></td>
             </tr>
@@ -109,18 +154,47 @@ function renderFiles() {
 
 // HASH BASED ON JSON CONTENT
 function contentHash(obj) {
-
-   // STRINGIFY
    var string = JSON.stringify(obj);
-
-   // FORCE LOWERCASE
-   var string = string.toLowerCase();
-
-   // TRIM SPACES
-   var string = string.trim();
-
-   // HASH
+   var string = vkbeautify.jsonmin(string);
    var string = md5(string);
 
    return string;
+}
+
+function fetchData(path, object) {
+   path = path.split('/');
+   var pathLen = path.length;
+   var content;
+
+   switch (pathLen) {
+      case 1:
+         content = object[path[0]];
+      break;
+
+      case 2:
+         content = object[path[0]][path[1]];
+      break;
+
+      case 3:
+         content = object[path[0]][path[1]][path[2]];
+      break;
+   }
+
+   return content;
+}
+
+// CLOSE PROMPT WINDOW
+function closePrompt() {
+
+   // CHECK CURRENT DISPLAY VALUE
+   var value = $("#prompt").css('display');
+
+   if (value == 'table') {
+
+      // CSS HIDE SELECTOR
+      $("#prompt").css('display', 'none');
+
+      // REMOVE FROM DOM
+      $('#prompt').remove();
+   }
 }

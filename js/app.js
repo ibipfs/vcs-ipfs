6550a78dbbb503f66dc0da1afc2340b6a5070117
wickstjo <33825194+wickstjo@users.noreply.github.com>
2018-06-30 05:24:15 +0300

    MetaMask functionality done

--- js/app.js
+++ js/app.js
@@ -215,13 +215,6 @@ function promisify(query, value = null) {
                }
             });
          break;
-
-         // FETCH MEMBERS ARRAY
-         case 'members':
-            contract.fetchMembers((err, result) => {
-               resolve(result);
-            });
-         break;
  
          // FALLBACK
          default:
@@ -244,21 +237,51 @@ promisify('metamask').then((accounts) => {
    var bg;
 
    if (count == 1) {
-      text = 'MetaMask Connected';
-      bg = 'success';
+      var user = accounts[0];
 
-      // SET ACCOUNT AS SELECTOR KEY
-      $('#metamask').attr('key', accounts[0]);
+      // MEMBERS ARRAY -- REPLACE WITH BLOCKCHAIN VERSION
+      var members = ['0xf8ac66155f50a5abd84e663834e2bca064f360c4', '0x2ca50c5fb8d1667189706d589cb554ef882c8eff'];
+      
+      // CHECK IF MEMBER IS IN ARRAY
+      var check = $.inArray(user, members);
 
-      promisify('members').then((members) => {
-         log(members);
-      });
+      // IF USER IS FOUND
+      if (check != -1) {
 
+         // MEMBERSINFO OBJECT -- REPLACE WITH BLOCKCHAIN VERSION
+         var memberInfo = {
+            "0xf8ac66155f50a5abd84e663834e2bca064f360c4": {
+               name: "wickstjo",
+               timestamp: 1530321302
+            },
+            "0x2ca50c5fb8d1667189706d589cb554ef882c8eff": {
+               name: "testuser",
+               timestamp: 1530324655
+            },
+         }
+
+         // FETCH CORRESPONDING NAME
+         var name = memberInfo[user].name;
+
+         // SET ACCOUNT NAME AS SELECTOR KEY
+         $('#metamask').attr('key', name);
+         
+         text = 'MetaMask Connected - ' + name;
+         bg = 'success';
+
+      // IF CONNECTED BUT NOT A MEMBER
+      } else {
+         text = 'MetaMask Found - Anonymous';
+         bg = 'caution';
+      }
+
+   // IF METAMASK CANT BE FOUND
    } else {
-      text = 'MetaMask Not Connection';
+      text = 'MetaMask Not Found';
       bg = 'error';
    }
    
+   // FINALLY RENDER APPROPRIATELY
    $('#metamask').text(text);
    $('#metamask').css('background', "url('interface/img/" + bg + ".png')");
 });

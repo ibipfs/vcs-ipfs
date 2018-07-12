15ef316d659fb8297c349a73fd250b5096dafebe
BDA Lab <jfwick@gmail.com>
2018-07-12 12:57:22 +0300

    More bugfixing

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-// REQUIRE IN FOR MUTABLE IPFS METHODS
-var Mutable = require('./classes/mutable.js');
-
 // old = QmUfiUfPtE8Yv6tim9bFSanQrZMJR7NgH4ACgeU2zbQwJE
 var validCID = 'QmaYEbgsnsGdWG9oPBrM6L3ZqVpCLJMUBHQL4ds424QHu9';
 var render = new Render(validCID);
@@ -8,14 +5,15 @@ var render = new Render(validCID);
 render.body();
 render.footer();
 
-var mutable = new Mutable();
+// REQUIRE IN FOR MUTABLE IPFS METHODS
+//var Mutable = require('./classes/mutable.js');
 
-mutable.list();
-mutable.check();
+// var mutable = new Mutable();
+
+// mutable.list();
+// mutable.check();
 
 // mutable.write('asdf.js', 'asdasd').then(() => {
 //    mutable.flush();
 //    mutable.list();
 // });
-
-//mutable.read('history.json');

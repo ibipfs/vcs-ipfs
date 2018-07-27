61a8d3aa6daec30075b8d20f267f273e91300f89
BDA Lab <jfwick@gmail.com>
2018-07-27 10:41:51 +0300

    Bugfixing in release func

--- js/classes/actions.js
+++ js/classes/actions.js
@@ -17,84 +17,150 @@ class Actions {
          // PARSE STRING TO OBJ
          history = JSON.parse(history);
 
+         // CURRENT ROOT DIR
+         var base = history.current.hash;
+
          // FETCH CONTENT OF CURRENT VERSION
          promisify('get', history.current.hash).then((content) => {
             
-            // ASSIST VARS
-            var files = [];
-            var obj = {};
+            // READ TRACKER
+            mutable.read('tracker.json').then((tracker) => {
+               tracker = JSON.parse(tracker);
 
-            // LOOP THROUGH EACH ENTRY
-            content.forEach(entry => {
+               var trackerKeys = Object.keys(tracker);
+               var edited_names = [];
+               var edited_content = [];
 
-               // IF ENTRY IS A FILE
-               if (entry.content != undefined) {
-                  log('File found!');
+               // LOOP THROUGH OBJECT
+               trackerKeys.forEach(entry => {
 
-                  // GENERATE OBJECT FOR ENTRY
-                  obj = {
-                     path: entry.path,
-                     content: Buffer.from('asdaasd')
-                  }
+                  // FORMAT PATH
+                  var path = tracker[entry].path;
+                  path = path.split('/');
+                  path[0] = base;
+                  path = path.join('/');
+                  
+                  // PUSH INTO EDITED ARRAY
+                  edited_names.push(path.toLowerCase());                      // FORCE LOWERCASE
+                  edited_content.push(tracker[entry]['wickstjo'].hash);
+               });
 
-                  // SAVE OBJECT INTO ARRAY
-                  files.push(obj);
-               }
-            });
+               // CONTENT PROMISES
+               var content_promises = [];
 
-            // ADD CONSTRUCTED DIR TO IPFS
-            mutable.release(files).then((response) => {
+               // CREATE PROMISE FOR FILE
+               edited_content.forEach(entry => {
+                  
+                  // GENERATE PROMISE
+                  var promise = promisify('file', entry);
 
-               // FETCH ROOT DIR HASH
-               var root = response[response.length - 1].hash;
+                  // PUSH INTO ARRAY
+                  content_promises.push(promise);
+               });
 
-               // ASSESS NEW NAME
-               var old_name = parseFloat(history.current.name);
-               var new_name = '';
+               // WAIT FOR ALL PROMISES TO BE RESOLVED
+               Promise.all(content_promises).then((edited_values) => {
+                  log(edited_values);
 
-               switch(significance) {
+                  // ASSIST VARS
+                  var files = [];
+                  var obj = {};
 
-                  // MEDIUM
-                  case 'medium':
-                     new_name = old_name + 0.1;
-                     new_name = new_name.toFixed(1);
-                  break;
+                  // LOOP THROUGH EACH ENTRY
+                  content.forEach(entry => {
 
-                  // LARGE
-                  case 'large':
-                     if (old_name % 1 != 0) { 
-                        new_name = Math.ceil(old_name);
-                     } else {
-                        new_name = old_name + 1;
-                        new_name = new_name.toFixed(1);
+                     // IF ENTRY IS A FILE
+                     if (entry.content != undefined) {
+                        
+                        // CHECK IF MATCH IS FOUND
+                        var indexCheck = $.inArray(entry.path.toLowerCase(), edited_names);
+                        var content = '';
+
+                        // MATCH FOUND
+                        if (indexCheck != -1) {
+                           log(entry.path + ' needs to be edited!');
+
+                           // FIX CASE SENSITIVE ISSUES
+                           if (entry.path != edited_names[indexCheck]) {
+                              edited_names[indexCheck] = entry.pathM
+                           }
+
+                           // REWRITE FILE
+                           content = edited_values[indexCheck];
+
+                        // FALLBACK
+                        } else {
+
+                           // KEEP OLD CONTENT
+                           content = entry.content;
+                        }
+
+                        // GENERATE OBJECT FOR ENTRY
+                        obj = {
+                           path: entry.path,
+                           content: Buffer.from(content)
+                        }
+
+                        // SAVE OBJECT INTO ARRAY
+                        files.push(obj);
                      }
-                  break;
+                  });
 
-                  // SMALL & FALLBACK
-                  default:
-                     new_name = old_name + 0.01;
-                     new_name = new_name.toFixed(2);
-                  break;
-               }
+                  // ADD CONSTRUCTED DIR TO IPFS
+                  mutable.release(files).then((response) => {
 
-               // CONVERT TO STRING
-               new_name = new_name.toString();
+                     // FETCH ROOT DIR HASH
+                     var root = response[response.length - 1].hash;
 
-               // MODIFY HISTORY LOG
-               history.old[history.current.name] = history.current;
-               history.current = {
-                  name: new_name,
-                  hash: root,
-                  timestamp: unixTime()
-               }
+                     // ASSESS NEW NAME
+                     var old_name = parseFloat(history.current.name);
+                     var new_name = '';
 
-               // STRINGIFY
-               history = JSON.stringify(history);
+                     switch(significance) {
 
-               // ADD CHANGES TO HISTORY LOG
-               mutable.write('history.json', history).then(() => {
-                  log('Rewrote history!');
+                        // MEDIUM
+                        case 'medium':
+                           new_name = old_name + 0.1;
+                           new_name = new_name.toFixed(1);
+                        break;
 
+                        // LARGE
+                        case 'large':
+                           if (old_name % 1 != 0) { 
+                              new_name = Math.ceil(old_name);
+                           } else {
+                              new_name = old_name + 1;
+                              new_name = new_name.toFixed(1);
+                           }
+                        break;
+
+                        // SMALL & FALLBACK
+                        default:
+                           new_name = old_name + 0.01;
+                           new_name = new_name.toFixed(2);
+                        break;
+                     }
+
+                     // CONVERT TO STRING
+                     new_name = new_name.toString();
+
+                     // MODIFY HISTORY LOG
+                     history.old[history.current.name] = history.current;
+                     history.current = {
+                        name: new_name,
+                        hash: root,
+                        timestamp: unixTime()
+                     }
+
+                     // STRINGIFY
+                     history = JSON.stringify(history);
+
+                     // ADD CHANGES TO HISTORY LOG
+                     mutable.write('history.json', history).then(() => {
+                        log('Rewrote history!');
+
+                     });
+                  });
                });
             });
          });

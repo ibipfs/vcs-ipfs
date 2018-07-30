4190a3596f401e16b45a798cba31a9978cdda8f5
BDA Lab <jfwick@gmail.com>
2018-07-30 15:21:32 +0300

    Migrated rest of immutable funcs

--- js/classes/immutable.js
+++ js/classes/immutable.js
@@ -17,6 +17,20 @@ class Immutable {
          });
       });
    }
+
+   // SIMILAR TO FILE METHOD, BUT FETCHES BINARY DATA
+   raw (value) {
+      ipfs.files.cat(value, function (err, file) {
+         resolve(file);
+      });
+   }
+
+   // OVERVIEW OF DIR OR FILE
+   get (value) {
+      ipfs.files.get(value, function (err, content) {
+         resolve(content);
+      });
+   }
 }
 
 // EXPORT CLASS

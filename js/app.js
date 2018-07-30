318a6984ba79fa9248c72f38df909563b34191ce
BDA Lab <jfwick@gmail.com>
2018-07-30 08:59:23 +0300

    Added config

--- js/app.js
+++ js/app.js
@@ -1,55 +1,57 @@
-var config = require('./config.js')();
-log(config)
+// ACCESS CONFIG MODULE
+require('./config.js')().then((config) => {
 
-// RENDER IN INDEX WHEN PAGE IS FIRST LOADED
-require('./content/index.js')();
-var current = 'files';
+   // RENDER IN INDEX WHEN PAGE IS FIRST LOADED
+   require('./content/index.js')();
+   var current = 'files';
 
-// WHEN MENULINK IS CLICKED
-$('body').on('click', '#menu a', () => {
- 
-   // CLICKED MENU ELEMENT VALUE
-   var val = $(event.target).text();
-   val = val.toLowerCase();
- 
-   if (current != val) {
-
-      // REMOVE UNDERLINE FROM EVERY ELEMENT
-      $('#menu a').css('text-decoration', 'none');
-
-      // UNDERLINE TARGET
-      $(event.target).css('text-decoration', 'underline');
-
-      // PLACEHOLDER MODULE
-      var sectionModule;
+   // WHEN MENULINK IS CLICKED
+   $('body').on('click', '#menu a', () => {
    
-      switch (val) {
+      // CLICKED MENU ELEMENT VALUE
+      var val = $(event.target).text();
+      val = val.toLowerCase();
    
-         // ACTIVITY
-         case 'activity':
-            sectionModule = require('./content/activity.js');
-            current = 'activity';
-         break;
+      if (current != val) {
 
-         // TRACKER
-         case 'tracker':
-            sectionModule = require('./content/tracker.js');
-            current = 'tracker';
-         break;
+         // REMOVE UNDERLINE FROM EVERY ELEMENT
+         $('#menu a').css('text-decoration', 'none');
 
-         // TRACKER
-         case 'actions':
-            sectionModule = require('./content/actions.js');
-            current = 'actions';
-         break;
-   
-         // FALLBACK & INDEX
-         default:
-            sectionModule = require('./content/index.js');
-            current = 'files';
-         break;
+         // UNDERLINE TARGET
+         $(event.target).css('text-decoration', 'underline');
+
+         // PLACEHOLDER MODULE
+         var sectionModule;
+      
+         switch (val) {
+      
+            // ACTIVITY
+            case 'activity':
+               sectionModule = require('./content/activity.js');
+               current = 'activity';
+            break;
+
+            // TRACKER
+            case 'tracker':
+               sectionModule = require('./content/tracker.js');
+               current = 'tracker';
+            break;
+
+            // TRACKER
+            case 'actions':
+               sectionModule = require('./content/actions.js');
+               current = 'actions';
+            break;
+      
+            // FALLBACK & INDEX
+            default:
+               sectionModule = require('./content/index.js');
+               current = 'files';
+            break;
+         }
+
+         sectionModule();
       }
+   });
 
-      sectionModule();
-   }
 });
--- js/config.js
+++ js/config.js
@@ -1,16 +1,34 @@
+// FETCH & INSTANTIATE MUTABLE MODULE
 var Mutable = require('./classes/mutable.js');
 var mutable = new Mutable();
 
 function build() {
-   var config = {};
 
-   mutable.read('tracker.json').then((tracker) => {
-      tracker = JSON.parse(tracker);
+   // GENERATE PROMISES
+   var history = mutable.read('history.json');
+   var tracker = mutable.read('tracker.json');
+   var log = mutable.read('log.json');
+
+   // WAIT FOR ALL PROMISES TO BE RESOLVED
+   return Promise.all([history, tracker, log]).then((values) => {
+
+      // PARSE LOGS
+      history = JSON.parse(values[0]);
+      tracker = JSON.parse(values[1]);
+      log = JSON.parse(values[2]);
+
+      // CREATE CONFIG OBJECT
+      var config = {};
+
+      // ADD LOGS TO CONFIG
+      config.history = history;
       config.tracker = tracker;
+      config.log = log;
 
+      // RETURN CONFIG
       return config;
    });
 }
 
-// EXPORT CLASS
+// EXPORT FUNCTION
 module.exports = build;

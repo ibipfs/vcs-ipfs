040e6c14852fff0dc4bd27eacd876fb4ae3bd5b9
BDA Lab <jfwick@gmail.com>
2018-07-30 09:48:43 +0300

    config tweaking

--- js/app.js
+++ js/app.js
@@ -1,57 +1,52 @@
-// ACCESS CONFIG MODULE
-require('./config.js')().then((config) => {
+// RENDER IN INDEX WHEN PAGE IS FIRST LOADED
+require('./content/index.js')();
+var current = 'files';
 
-   // RENDER IN INDEX WHEN PAGE IS FIRST LOADED
-   require('./content/index.js')();
-   var current = 'files';
+// WHEN MENULINK IS CLICKED
+$('body').on('click', '#menu a', () => {
 
-   // WHEN MENULINK IS CLICKED
-   $('body').on('click', '#menu a', () => {
+   // CLICKED MENU ELEMENT VALUE
+   var val = $(event.target).text();
+   val = val.toLowerCase();
+
+   if (current != val) {
+
+      // REMOVE UNDERLINE FROM EVERY ELEMENT
+      $('#menu a').css('text-decoration', 'none');
+
+      // UNDERLINE TARGET
+      $(event.target).css('text-decoration', 'underline');
+
+      // PLACEHOLDER MODULE
+      var sectionModule;
    
-      // CLICKED MENU ELEMENT VALUE
-      var val = $(event.target).text();
-      val = val.toLowerCase();
+      switch (val) {
    
-      if (current != val) {
+         // ACTIVITY
+         case 'activity':
+            sectionModule = require('./content/activity.js');
+            current = 'activity';
+         break;
 
-         // REMOVE UNDERLINE FROM EVERY ELEMENT
-         $('#menu a').css('text-decoration', 'none');
+         // TRACKER
+         case 'tracker':
+            sectionModule = require('./content/tracker.js');
+            current = 'tracker';
+         break;
 
-         // UNDERLINE TARGET
-         $(event.target).css('text-decoration', 'underline');
-
-         // PLACEHOLDER MODULE
-         var sectionModule;
-      
-         switch (val) {
-      
-            // ACTIVITY
-            case 'activity':
-               sectionModule = require('./content/activity.js');
-               current = 'activity';
-            break;
-
-            // TRACKER
-            case 'tracker':
-               sectionModule = require('./content/tracker.js');
-               current = 'tracker';
-            break;
-
-            // TRACKER
-            case 'actions':
-               sectionModule = require('./content/actions.js');
-               current = 'actions';
-            break;
-      
-            // FALLBACK & INDEX
-            default:
-               sectionModule = require('./content/index.js');
-               current = 'files';
-            break;
-         }
-
-         sectionModule();
+         // TRACKER
+         case 'actions':
+            sectionModule = require('./content/actions.js');
+            current = 'actions';
+         break;
+   
+         // FALLBACK & INDEX
+         default:
+            sectionModule = require('./content/index.js');
+            current = 'files';
+         break;
       }
-   });
 
+      sectionModule();
+   }
 });
--- js/classes/activities.js
+++ js/classes/activities.js
@@ -1,6 +1,5 @@
 var Mutable = require('./mutable.js');
 var mutable = new Mutable();
-
 var moment = require('moment');
 
 class Activities {
@@ -16,107 +15,110 @@ class Activities {
 
    // RENDER BODY
    body(filter = '') {
-      this.init().then((content) => {
-         mutable.read('history.json').then((history) => {
+      require('../config.js')().then((config) => {
 
-            // PARSE HISTORY & FETCH HASH OF PROJ ROOT
-            history = JSON.parse(history);
-            var base = history.current.hash;
+         this.init().then((content) => {
+            mutable.read('history.json').then((history) => {
 
-            // PARSE LOG
-            var logz = JSON.parse(content);
+               // PARSE HISTORY & FETCH HASH OF PROJ ROOT
+               history = JSON.parse(history);
+               var base = history.current.hash;
 
-            // FETCH & REVERSE KEYS
-            var keys = Object.keys(logz);
-            keys.reverse();
+               // PARSE LOG
+               var logz = JSON.parse(content);
 
-            // HELP ARRS
-            var rows = '';
-            var row = '';
-            var table = '';
+               // FETCH & REVERSE KEYS
+               var keys = Object.keys(logz);
+               keys.reverse();
 
-            var type = '';
-            var timestamp = 0;
-            var user = '';
-            var original = '';
-            var path = '';
-            var realPath = '';
-            var hash = '';
+               // HELP ARRS
+               var rows = '';
+               var row = '';
+               var table = '';
 
-            var string = '';
+               var type = '';
+               var timestamp = 0;
+               var user = '';
+               var original = '';
+               var path = '';
+               var realPath = '';
+               var hash = '';
 
-            // MAKE ROW FOR EACH ENTRY
-            for (var x = 0; x < keys.length; x++) {
+               var string = '';
 
-               // FETCH VALUES
-               type = logz[keys[x]].type;
-               timestamp = moment.unix(keys[x]).format('D/MM @ HH:mm');
-               user = logz[keys[x]].user;
-               original = logz[keys[x]].original;
+               // MAKE ROW FOR EACH ENTRY
+               for (var x = 0; x < keys.length; x++) {
 
-               // SHORTHAND PATH
-               path = logz[keys[x]].path;
+                  // FETCH VALUES
+                  type = logz[keys[x]].type;
+                  timestamp = moment.unix(keys[x]).format('D/MM @ HH:mm');
+                  user = logz[keys[x]].user;
+                  original = logz[keys[x]].original;
 
-               // REAL PATH
-               realPath = path.split('/');
-               realPath[0] = base;
-               realPath = realPath.join('/');
+                  // SHORTHAND PATH
+                  path = logz[keys[x]].path;
 
-               // FILE HASH
-               hash = logz[keys[x]].hash;
+                  // REAL PATH
+                  realPath = path.split('/');
+                  realPath[0] = base;
+                  realPath = realPath.join('/');
 
-               // FILENAME FOR FILTER QUERY
-               var suffix = path.split('/').pop();
+                  // FILE HASH
+                  hash = logz[keys[x]].hash;
 
-               // CONTINUE IF A REQUIREMENT IS FILLED
-               if (filter == '' || filter.toLowerCase() == user.toLowerCase() || filter == original || filter == path || filter.toLowerCase() == suffix) {
+                  // FILENAME FOR FILTER QUERY
+                  var suffix = path.split('/').pop();
 
-                  // GENERATE ENTRY STRING
-                  switch (type) {
-                     
-                     // PUBLISH
-                     case 'publish':
-                        string = capitalize(user) + ' published an entry of <a id="compare" old="' + original + '" new="' + hash + '" author="' + user + '" path="' + realPath + '" time="' + timestamp + '">' + path + '</a>';
-                     break;
+                  // CONTINUE IF A REQUIREMENT IS FILLED
+                  if (filter == '' || filter.toLowerCase() == user.toLowerCase() || filter == original || filter == path || filter.toLowerCase() == suffix) {
+
+                     // GENERATE ENTRY STRING
+                     switch (type) {
+                        
+                        // PUBLISH
+                        case 'publish':
+                           string = capitalize(user) + ' published an entry of <a id="compare" old="' + original + '" new="' + hash + '" author="' + user + '" path="' + realPath + '" time="' + timestamp + '">' + path + '</a>';
+                        break;
+                     }
+
+                     // GENERATE ROW
+                     row = `
+                        <tr><td><div>
+                           <table><tbody><tr>
+                              <td>` + string + `</td>
+                              <td>` + timestamp + `</td>
+                           </tr></tbody></table>
+                        </div></td></tr>
+                     `;
+
+                     // CONCAT TO ROWS
+                     rows += row;
                   }
-
-                  // GENERATE ROW
-                  row = `
-                     <tr><td><div>
-                        <table><tbody><tr>
-                           <td>` + string + `</td>
-                           <td>` + timestamp + `</td>
-                        </tr></tbody></table>
-                     </div></td></tr>
-                  `;
-
-                  // CONCAT TO ROWS
-                  rows += row;
                }
-            }
 
-            // FALLBACK IF NO ROWS ARE FOUND
-            if (rows == '') {
-               rows = '<tr><td><div>No entries found.</div></td></tr>';
-            }
+               // FALLBACK IF NO ROWS ARE FOUND
+               if (rows == '') {
+                  rows = '<tr><td><div>No entries found.</div></td></tr>';
+               }
 
-            // GENERATE FULL TABLE
-            table = '<table><tbody>' + rows + '</tbody></table>';
-            var cont = $('#activity').html();
+               // GENERATE FULL TABLE
+               table = '<table><tbody>' + rows + '</tbody></table>';
+               var cont = $('#activity').html();
 
-            // COMPARE OLD AND NEW SELECTOR CONTENT
-            if (table == cont) {
+               // COMPARE OLD AND NEW SELECTOR CONTENT
+               if (table == cont) {
 
-               // RENDER TO SELECTOR
-               $('#activity').html(table);
+                  // RENDER TO SELECTOR
+                  $('#activity').html(table);
 
-            // WHEN CONTENT HAS CHANGED
-            } else {
+               // WHEN CONTENT HAS CHANGED
+               } else {
 
-               // FADE IN
-               fadeIn('activity', table);
-            }
+                  // FADE IN
+                  fadeIn('activity', table);
+               }
 
+            });
          });
       });
    }
